47c19dde542a774dfeb64cd0e3a2eaa1d3fe45d1
landson90 <randelsilva1990@gmail.com>
2022-01-22 17:54:09 -0300

    criando o componente transaction table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styled";
 export function Dashboard() {
     return(
         <Container>
             <Summary />
+            <TransactionsTable />
         </Container>
     );
 }
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styled";
+
+export function TransactionsTable() {
+    return(
+        <Container >
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Pj do Caema</td>
+                        <td className="deposit">R$:3800.0</td>
+                        <td>Entrada</td>
+                        <td>16/03/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Pj do Caema</td>
+                        <td className="withdraw">R$:3800.0</td>
+                        <td>Entrada</td>
+                        <td>16/03/2022</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    );
+}
