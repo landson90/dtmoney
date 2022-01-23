b0c259d329c21fd34a3a61b0ac502652394f0ecb
landson90 <randelsilva1990@gmail.com>
2022-01-23 08:05:03 -0300

    criando o componente tabela

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,6 +1,8 @@
 import { Summary } from "../Summary";
 import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styled";
+
+
 export function Dashboard() {
     return(
         <Container>
