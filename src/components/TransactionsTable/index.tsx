2669cd4eb4396dc44fde5ecce356613f781e74a9
landson90 <randelsilva1990@gmail.com>
2022-01-25 16:56:05 -0300

    listando dados da API

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,32 +1,51 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styled";
 
+interface ITransaction {
+    id: number,
+    title: string,
+    amount: number,
+    type: string,
+    category: string,
+    createAt: string
+}
 export function TransactionsTable() {
-    return(
-        <Container >
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Pj do Caema</td>
-                        <td className="deposit">R$:3800.0</td>
-                        <td>Entrada</td>
-                        <td>16/03/2022</td>
-                    </tr>
-                    <tr>
-                        <td>Pj do Caema</td>
-                        <td className="withdraw">-R$:3800.0</td>
-                        <td>Entrada</td>
-                        <td>16/03/2022</td>
-                    </tr>
-                </tbody>
-            </table>
-        </Container>
-    );
+  const [transactions, setTransactions] = useState<ITransaction[]>([]);
+
+  useEffect(() => {
+    api.get("/transactions").then((response) => {
+        console.log(response)
+        setTransactions(response.data.transactions)
+    });
+  }, []);
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => {
+            return (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                }).format(transaction.amount)}</td>
+                <td>{transaction.category}</td>
+                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createAt))}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Container>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,33 @@ createServer({
   models: {
     transaction: Model 
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Pj da vetor',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 12000,
+          createAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Declaração inposto',
+          type: 'withdraw',
+          category: 'Empresa',
+          amount: 2000,
+          createAt: new Date('2021-02-12 09:00:00'),
+        }
+      ]
+    })
+  },
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return this.schema.all('transactions')
+      return this.schema.all('transaction')
     });
     this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody);
