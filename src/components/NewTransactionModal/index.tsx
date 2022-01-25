32a41c90a63718ebb5704922a0824dfd435fd35b
landson90 <randelsilva1990@gmail.com>
2022-01-25 08:31:45 -0300

    termino dos botoes no modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -41,6 +41,7 @@ export function NewTransactionModal({
             type="button" 
             onClick={() => setType('deposit')}
             isActive={type === 'deposit'}
+            octiveColor="green"
             >
 
             <img src={icomeImg} alt="Entrada"/>
@@ -50,6 +51,7 @@ export function NewTransactionModal({
             type="button" 
             onClick={() => setType('withdraw')}
             isActive={type === 'withdraw'}
+            octiveColor="red"
             >
             <img src={outcomeImg} alt="Saída"/>
             <span>Saída</span>
--- src/components/NewTransactionModal/styled.ts
+++ src/components/NewTransactionModal/styled.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -60,21 +60,28 @@ export const TransactionTypeContainer = styled.div`
 
 interface IRadioBoxProps {
     isActive: boolean;
+    octiveColor: 'green' | 'red';
 }
+
+const colors = {
+    green: '#33CC95',
+    red: '#E52E4D'
+}
+
 export const RadioBox = styled.button<IRadioBoxProps>`
 
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
     
-    background: ${(props) => props.isActive ? '#eee' : 'transparent'};
+    background: ${(props) => props.isActive ? transparentize(0.9, colors[props.octiveColor]) : 'transparent'};
 
     display: flex;
     align-items: center;
     justify-content: center;
 
     transition: border-color: 0.2s;
-
+    
     &:hover {
         border-color: ${darken(0.1, '#d7d7d7')}
     }
