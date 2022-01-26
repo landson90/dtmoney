aaaa6ae8040f1a793741cc4e47a268115b0f9c3d
landson90 <randelsilva1990@gmail.com>
2022-01-25 22:23:23 -0300

    mostrando dados no card

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,7 +8,20 @@ import { Container } from "./styled";
 
 export function Summary() {
     const { transactions } = useContext(TransactionsContext);
-   
+   const summary = transactions.reduce((acc, transaction) => {
+    if(transaction.type === 'deposit') {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+    } else {
+        acc.withdraws += transaction.amount;
+        acc.total += transaction.amount;
+    }
+    return acc;
+   }, {
+       deposits: 0,
+       withdraws: 0,
+       total: 0
+   })
     return(
         <Container >
             <div>
@@ -16,21 +29,21 @@ export function Summary() {
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Entradas" />
                 </header>
-                <strong>R$:1000,00</strong>
+                <strong>{summary.deposits}</strong>
             </div>
             <div>
                 <header>
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Saidas" />
                 </header>
-                <strong>- R$:100,00</strong>
+                <strong>- {summary.withdraws}</strong>
             </div>
             <div className="highlight-background">
                 <header>
                     <p>Entradas</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong>R$:900,00</strong>
+                <strong>{summary.total}</strong>
             </div>
         </Container>
        
